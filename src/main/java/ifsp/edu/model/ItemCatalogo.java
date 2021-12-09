1ff532dbcdc96bac51b5116509baad804b52be98
Lucca Beraldo <lucca.beraldo@aluno.ifsp.edu.br>
2021-12-08 23:38:40 -0300

    criação e persistência da entidade ItemCatalogo no banco de dados.

--- src/main/java/ifsp/edu/model/ItemCatalogo.java
+++ src/main/java/ifsp/edu/model/ItemCatalogo.java
@@ -4,10 +4,32 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 public class ItemCatalogo {
-    private Catalogo catalago;
+    private Catalogo catalogo;
     private Produto produto;
     private Double valor;
 
 
+    public Catalogo getCatalogo() {
+        return catalogo;
+    }
 
+    public void setCatalogo(Catalogo catalogo) {
+        this.catalogo = catalogo;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
 }
---
+++ src/main/java/ifsp/edu/sqlitedao/ItemCatalogoDAOImpl.java
@@ -0,0 +1,84 @@
+package ifsp.edu.sqlitedao;
+
+import ifsp.edu.model.Catalogo;
+import ifsp.edu.model.Item;
+import ifsp.edu.model.ItemCatalogo;
+import ifsp.edu.model.Produto;
+import ifsp.edu.usecases.catalogo.CatalogoDAO;
+import ifsp.edu.usecases.produto.ProdutoDAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemCatalogoDAOImpl {
+
+
+    public boolean insert(ItemCatalogo itemCatalogo){
+        String sql = "INSERT INTO ITEMCATALOGO(id_catalogo, id_produto, valor) VALUES( ?, ?, ?)";
+        try(PreparedStatement ps = ConnectionFactory.criarPreparedStatement(sql)) {
+            ps.setInt(1, itemCatalogo.getCatalogo().getId());
+            ps.setInt(2, itemCatalogo.getProduto().getId());
+            ps.setDouble(3, itemCatalogo.getValor());
+            ps.execute();
+
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+
+    public List<ItemCatalogo> listItemsByIdCatalogo(Integer idCatalogo) {
+        String sql = "SELECT * FROM ITEMCATALOGO WHERE ID_CATALOGO = ?";
+        List<ItemCatalogo> items = new ArrayList<>();
+        try (PreparedStatement ps = ConnectionFactory.criarPreparedStatement(sql)) {
+            ps.setInt(1, idCatalogo);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                ProdutoDAO daoProduto = new ProdutoDAOImpl();
+                Produto p = daoProduto.findOne(rs.getInt("ID_PRODUTO"));
+
+                CatalogoDAO daoCatalogo = new CatalogoDAOImpl();
+                Catalogo c = daoCatalogo.findOne(rs.getInt("ID_CATALOGO"));
+
+                ItemCatalogo itemCatalogo = new ItemCatalogo(c,p,rs.getDouble("VALOR"));
+
+                items.add(itemCatalogo);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return items;
+
+    }
+
+    public boolean deleteCatalogo(Integer idCatalogo) {
+        String sql = "DELETE FROM ITEMCATALOGO WHERE id_catalogo = ?";
+        try(PreparedStatement ps = ConnectionFactory.criarPreparedStatement(sql)) {
+            ps.setInt(1, idCatalogo);
+            ps.execute();
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public boolean deleteItemCatalogo(ItemCatalogo itemCatalogo) {
+        String sql = "DELETE FROM ITEMCATALOGO WHERE id_catalogo = ? and id_produto = ?";
+        try(PreparedStatement ps = ConnectionFactory.criarPreparedStatement(sql)) {
+            ps.setInt(1, itemCatalogo.getCatalogo().getId());
+            ps.setInt(1, itemCatalogo.getProduto().getId());
+            ps.execute();
+            return true;
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
